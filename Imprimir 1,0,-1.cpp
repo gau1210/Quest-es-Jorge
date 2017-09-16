80efee26f9b841cacd4b0635fa61958b670c7c28
Glauber <gau1210@gmail.com>
2017-09-16 11:25:40 -0300

    Add files via upload

---
+++ Imprimir 1,0,-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void){
+    
+    int n;
+    
+    scanf("%d",&n);
+    
+    printf("%d",(n>0)-(n<0));
+    
+    system("pause");
+    return 0;
+    
+}
