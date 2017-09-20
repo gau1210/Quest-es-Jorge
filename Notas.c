0c2b69d5ea1977b79ee0b2c1ca684df2ff37e718
Glauber <gau1210@gmail.com>
2017-09-20 03:11:33 -0300

    Add files via upload

---
+++ Notas.c
@@ -0,0 +1,27 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(void)
+{
+    float valor;
+	//inicilizo o vetor com as notas em ordem do maior para o menor;
+    float notas[] = { 100, 50, 20, 10, 5, 2, 1 };
+	int i = 0;
+    int quantidade;
+	
+	printf("informe um valor");
+	scanf("%f", &valor);
+	
+    printf("NOTAS:\n");
+	
+	//Emquanto o valor não for 0 verifica se a outra nota até o valor;
+	 while(notas[i] != 0) {
+        quantidade = valor / notas[i];//divide o valor para saber a quant de notas
+        printf("%d nota(s) de R$ %.2f\n", quantidade, notas[i]);
+        valor -= quantidade * notas[i];//multiplica a quantidade pela notase subitrai do valor;
+
+        i++;
+    }
+	 system("pause");
+    return 0;
+}
