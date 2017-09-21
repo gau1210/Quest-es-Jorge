b5ca90f90b18179ed129511065f89c36155f62f6
Glauber <gau1210@gmail.com>
2017-09-21 08:46:43 -0300

    Update Notas.c

--- Notas.c
+++ Notas.c
@@ -4,7 +4,7 @@
 int main(void)
 {
     float valor;
-	//inicilizo o vetor com as notas em ordem do maior para o menor;
+	//Iniciliza o vetor com as notas em ordem do maior para o menor;
     float notas[] = { 100, 50, 20, 10, 5, 2, 1 };
 	int i = 0;
     int quantidade;
@@ -16,9 +16,9 @@ int main(void)
 	
 	//Emquanto o valor não for 0 verifica se a outra nota até o valor;
 	 while(notas[i] != 0) {
-        quantidade = valor / notas[i];//divide o valor para saber a quant de notas
+        quantidade = valor / notas[i];//divide o valor para saber a quantidade de notas
         printf("%d nota(s) de R$ %.2f\n", quantidade, notas[i]);
-        valor -= quantidade * notas[i];//multiplica a quantidade pela notase subitrai do valor;
+        valor -= quantidade * notas[i];//multiplica a quantidade pelas notas e subitrai o valor;
 
         i++;
     }
