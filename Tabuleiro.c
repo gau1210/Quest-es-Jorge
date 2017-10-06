d568da3da512b223284543611bd4ebfed2b25374
Glauber <gau1210@gmail.com>
2017-10-06 02:26:50 -0300

    Add files via upload

---
+++ Tabuleiro.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(){
+
+	int a,b,v,t=0;
+
+	scanf("%d %d",&a,&b);
+
+	v=(a+b);
+	t=v%2;
+
+	switch (t){
+
+		case 0:
+
+		return 0;
+
+		case 1:
+
+		return 1;
+
+	}
+
+	system("pause");
+	return 0;
+}
