2f27bd7a8623307051d7588d3d5313732d0ab07b
Glauber <gau1210@gmail.com>
2017-10-12 23:21:20 -0300

    Add files via upload

---
+++ Soma de matriz.c
@@ -0,0 +1,41 @@
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(){
+
+	int i,j,m,n;
+
+	scanf("%d %d",&m,&n);
+
+	int a[2][3],b[2][3],c[2][3];
+
+	for(i=0; i<m; i++){
+
+		for(j=0; j<n; j++){
+
+			scanf("%d",&a[i][j]);
+
+	for(i=0; i<m; i++){
+
+		for(j=0; j<n; j++){
+
+			scanf("%d",&b[i][j]);
+
+		        }
+		    }
+		}
+	}
+
+	for(i=0; i<m; i++){
+
+		for(j=0; j<n; j++){
+
+			c[i][j]=a[i][j]+b[i][j];
+			printf("%d",c[i][j]);
+
+		}
+		printf("\n");
+	}
+system("pause");
+return 0;
+}
